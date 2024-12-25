b21c6ebbf92647e89fa56face6d03df2d1cfcaa2
Emi_ <103583822+jerkey06@users.noreply.github.com>
2024-12-25 15:19:31 -0600

    Primer commit

--- obj/Debug/net9.0/Urlshort.AssemblyInfo.cs
+++ obj/Debug/net9.0/Urlshort.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Urlshort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b98e8cdcf0fb7c557b00e62b214236e2e991ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Urlshort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Urlshort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
